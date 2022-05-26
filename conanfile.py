9e18eec86a0d586d7526d12755b2efe4713cc1f0
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-05-26 12:30:26 +0000

    :wrench: Update environment.

--- conanfile.py
+++ conanfile.py
@@ -27,7 +27,7 @@ class CppHashTablesConan(ConanFile):
     def build_requirements(self):
         if self.options.requirements_for_tests:
             self.build_requires(
-                "catch2/3.0.0pre4@MusicScience37+conan-extra-packages/stable"
+                "catch2/3.0.1@MusicScience37+conan-extra-packages/stable"
             )
             self.build_requires("trompeloeil/42")
             self.build_requires(
--- scripts/update_source_list.py
+++ scripts/update_source_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Helper script to create lists of source codes for build.
 """
 
